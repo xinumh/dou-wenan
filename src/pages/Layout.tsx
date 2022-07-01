95f0e518be5562132bde57bdcbf713cdca7b442f
hx <huangxin@wondertek.com.cn>
2022-07-01 08:29:34 +0800

    feat: layout

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,7 +1,10 @@
-import { ReactElement, ReactNode } from "react";
+import React, { ReactElement, ReactNode } from "react";
 import { Link, Outlet } from "react-router-dom";
+import { Layout, Menu } from 'antd';
 
-const Layout = (): JSX.Element => {
+const { Header, Content, Footer, Sider } = Layout;
+
+const LayoutPage = (): JSX.Element => {
   return (
     <div>
       <nav>
@@ -21,8 +24,36 @@ const Layout = (): JSX.Element => {
       <hr />
 
       <Outlet />
+      <Layout>
+        <Sider
+          breakpoint="lg"
+          collapsedWidth="0"
+          onBreakpoint={broken => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
+        >
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={['4']}
+          />
+        </Sider>
+        <Layout>
+          <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
+          <Content style={{ margin: '24px 16px 0' }}>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+              content
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+      </Layout>
     </div>
   );
 }
 
-export default Layout
+export default LayoutPage
